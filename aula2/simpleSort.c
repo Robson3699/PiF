edfa2c59a776391eef0f7d1da5e1c44b79511746
Robson Sandro <robsonsandro@hotmail.com>
2025-03-16 11:16:21 -0300

    update simpleSort

--- aula2/simpleSort.c
+++ aula2/simpleSort.c
@@ -6,7 +6,6 @@ int main() {
    scanf("%d", &x);
    scanf("%d", &y);
    scanf("%d", &z);
-   int vect[3]={x,y,z};
    int sort[3]={x,y,z};
    for(int i=0;i<3;i++){
          for(int j=i+1;j<3;j++){
@@ -23,9 +22,9 @@ int main() {
    printf("%d\n",sort[1]);
    printf("%d\n",sort[2]);
    printf("\n");
-   printf("%d\n",vect[0]);
-   printf("%d\n",vect[1]);
-   printf("%d\n",vect[2]);
+   printf("%d\n",x);
+   printf("%d\n",y);
+   printf("%d\n",z);
    
        
    
