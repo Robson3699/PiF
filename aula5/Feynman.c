9a84b361964919c2ed41fc75d92710520aa5b304
Robson Sandro <robsonsandro@hotmail.com>
2025-04-08 20:53:05 -0300

    Feynman

--- aula5/Feynman.c
+++ aula5/Feynman.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+void Square(int n){
+    int side=0;
+    side=(n*(n+1)*(n*2+1))/6;
+    printf("%d\n",side);
+}
+
+typedef struct
+{
+    int n;
+}Feyman;
+
+
 int main(){
-    
+    int exit=1;
+
+    while(exit!=0){
+        Feyman fey;
+        scanf("%d",&fey.n);
+        if(fey.n==0){
+            exit=0;
+            break;
+        }
+        Square(fey.n);
+
+
+
+    }
+
+
     return 0;
 }
